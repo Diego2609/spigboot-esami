bfda615f242d7f3736ca3225917896802464f1df
Super <Super@LAPTOP-AH4TSE2A.station>
2020-07-10 11:11:30 +0200

    commit iniziale, creazione utenti e ruoli

---
+++ esami-springboot/src/main/java/it/dstech/formazione/models/Ruolo.java
@@ -0,0 +1,50 @@
+package it.dstech.formazione.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ruolo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idRuolo;
+	private String ruolo;
+	public Long getIdRuolo() {
+		return idRuolo;
+	}
+	public void setIdRuolo(Long idRuolo) {
+		this.idRuolo = idRuolo;
+	}
+	public String getRuolo() {
+		return ruolo;
+	}
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ruolo == null) ? 0 : ruolo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ruolo other = (Ruolo) obj;
+		if (ruolo == null) {
+			if (other.ruolo != null)
+				return false;
+		} else if (!ruolo.equals(other.ruolo))
+			return false;
+		return true;
+	}
+
+}
